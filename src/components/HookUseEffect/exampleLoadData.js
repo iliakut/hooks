6496d562ebd4e6e73e180d68c76f947ace8e6d48
ilia <ilyakut@gmail.com>
2020-05-24 22:00:46 +0300

    added useEffect exampleLoadData.js

--- src/components/HookUseEffect/exampleLoadData.js
+++ src/components/HookUseEffect/exampleLoadData.js
@@ -2,18 +2,28 @@ import React, {useEffect, useState} from "react";
 
 const ExampleLoadData = ({id}) => {
   const [name, setName] = useState(null);
-  const controller = new AbortController();
+  // const controller = new AbortController();
 
-  const loadData = async (id) => {
-    const resp = await fetch(`https://swapi.dev/api/planets/${id}`, {
-      signal: controller.signal
-    });
-    const data = await resp.json();
-    setName(data.name)
-  };
+  // const loadData = async (id) => {
+  //   const resp = await fetch(`https://swapi.dev/api/planets/${id}`, {
+  //     signal: controller.signal
+  //   });
+  //   const data = await resp.json();
+  //   setName(data.name)
+  // };
 
   useEffect(   () => {
-    loadData(id);
+    // loadData(id);
+    //
+    // return () => {
+    //   controller.abort();
+    // }
+    const controller = new AbortController();
+    fetch(`https://swapi.dev/api/planets/${id}`, {
+      signal: controller.signal
+    })
+      .then(res => res.json())
+      .then(data => setName(data.name));
 
     return () => {
       controller.abort();
